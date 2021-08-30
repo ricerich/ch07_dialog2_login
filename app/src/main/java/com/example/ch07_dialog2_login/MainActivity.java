369eb867b5cd1cb7fb99a9d09fe80228e0d76cf3
KJB <drv98@naver.com>
2021-08-30 14:12:09 +0900

    7장 로그인 다이얼로그

---
+++ app/src/main/java/com/example/ch07_dialog2_login/MainActivity.java
@@ -0,0 +1,103 @@
+package com.example.ch07_dialog2_login;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.Display;
+import android.view.Gravity;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvName, tvEmail;
+    Button button1;
+    EditText dlgEdtName, dlgEdtEmail;
+    TextView toastText;
+    View dialogView, toastView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("사용자 정보 입력");
+
+//        tvName = (TextView) findViewById(R.id.tvName);
+//        tvEmail = (TextView) findViewById(R.id.tvEmail);
+        tvName = (TextView) findViewById(R.id.edtName);
+        tvEmail = (TextView) findViewById(R.id.edtEmail);
+
+        button1 = (Button) findViewById(R.id.button1);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                dialogView = (View) View.inflate(MainActivity.this,
+                        R.layout.dialog1, null);
+
+                AlertDialog.Builder dlg = new AlertDialog.Builder(
+                        MainActivity.this);
+                dlg.setTitle("사용자 정보 입력");
+                dlg.setIcon(R.drawable.ic_menu_allfriends);
+                dlg.setView(dialogView);
+
+                dlgEdtName = (EditText) dialogView.findViewById(R.id.dlgEdt1);
+                dlgEdtEmail = (EditText) dialogView.findViewById(R.id.dlgEdt2);
+
+                String str1 = tvName.getText().toString();
+                String str2 = tvEmail.getText().toString();
+
+                dlgEdtName.setText(str1);
+                dlgEdtEmail.setText(tvEmail.getText().toString());
+
+                dlg.setPositiveButton("확인",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,
+                                                int which) {
+//                                dlgEdtName = (EditText) dialogView.findViewById(R.id.dlgEdt1);
+//                                dlgEdtEmail = (EditText) dialogView.findViewById(R.id.dlgEdt2);
+
+                                tvName.setText(dlgEdtName.getText().toString());
+                                tvEmail.setText(dlgEdtEmail.getText().toString());
+                                // dlgLayout = null;
+                            }
+                        });
+
+                dlg.setNegativeButton("취소",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,
+                                                int which) {
+                                Toast toast = new Toast(MainActivity.this);
+
+                                toastView = (View) View.inflate(
+                                                            MainActivity.this,
+                                                                    R.layout.toast1,
+                                                                    null);
+
+                                toastText = (TextView) toastView.findViewById(R.id.toastText1);
+                                toastText.setText("취소했습니다");
+                                toast.setView(toastView);
+
+                                Display display = ((WindowManager) getSystemService(WINDOW_SERVICE))
+                                        .getDefaultDisplay();
+                                int xOffset = (int) (Math.random() * display.getWidth());
+                                int yOffset = (int) (Math.random() * display.getHeight());
+
+                                toast.setGravity(Gravity.TOP | Gravity.LEFT,
+                                                    xOffset, yOffset);
+
+                                toast.show();
+                            }
+                        });
+                dlg.show();
+
+            }
+        });
+
+    }
+}
